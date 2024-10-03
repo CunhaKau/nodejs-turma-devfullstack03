c3d45b04479b0a9860268a049310d65cdccddd8c
Kau Cunha - @kau.tech <kau.cunha@hotmail.com>
2024-10-03 10:39:09 -0300

    Atualização

--- nodejs/modulos.js
+++ nodejs/modulos.js
@@ -6,3 +6,8 @@ console.log(executePessoas.pessoas, executePessoas.idade); */
 //desestruturação
 const {pessoas, idade} = require ('./pessoas');
 console.log(pessoas, idade);
+
+//descobrindo informações sobre o sistema operacional
+const sistemaOperacional = require('os');
+//console.log(sistemaOperacional);
+console.log(sistemaOperacional.platform(), sistemaOperacional.homedir());
