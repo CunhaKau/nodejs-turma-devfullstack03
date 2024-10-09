f2576ff05d65195f812ffb11c53a2d04663a3fff
Kau Cunha - @kau.tech <kau.cunha@hotmail.com>
2024-10-09 12:39:39 -0300

    atualização

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,15 +2,29 @@ const express = require ('express');
 
 //express aplicativo - configurando as funções
 const app = express();
+app.set('view engine', 'ejs');
 
-app.listen(3001);
+app.listen(3006);
 
 //acessando a rota
 app.get('/', (req, res) => {
-res.sendFile('./views/index.html', { root: __dirname});
+//res.sendFile('./views/index.html', { root: __dirname});
+res.render('index');
 });
 
+//nova rota
 app.get('/sobre', (req, res) => {
-    res.sendFile('./views/sobre.html', { root: __dirname});
+    //res.sendFile('./views/sobre.html', { root: __dirname});
+    res.render('sobre');
+});
+
+//redirecionamento de página
+app.get('/sobrenos', (req, res) => {
+    res.redirect('/sobre');
+});
+
+app.use((req, res) =>{
+//res.status(400).sendFile('./views/404.html', { root: __dirname});
+res.status(404).render('404');
 });
 
